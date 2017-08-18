5bf42d106ac4237265c3f0bba792b572a6b81c12
Presenter <presenter@ucmerced-10-33-21-230.ucmerced.edu>
2017-08-18 11:59:32 -0700

    added challenge, group_by, summarize and tally

--- tidyverse_intro.R
+++ tidyverse_intro.R
@@ -20,5 +20,35 @@ surveys_sml <- surveys %>%
 
 surveys_sml
 
+surveys %>%
+  filter(!is.na(weight)) %>%
+  mutate(weight_kg = weight / 1000,
+         weight_kg2 = weight_kg * 2) %>%
+  head
+
+## Challenge
+## Create a new data frame from the surveys data that 
+## meets the following criteria: contains only the species_id 
+## column and a new column called  hindfoot_half containing values 
+## that are half the hindfoot_length values. In this hindfoot_half
+## column,there are no NAs and all values are less than 30.
+
+challenge1 <- surveys %>%
+  mutate(hindfoot_half = 0.5 * hindfoot_length) %>%
+  select(species_id, hindfoot_half) %>%
+  filter(!is.na(hindfoot_half), hindfoot_half < 30)
+
+## group_by and summarize
+
+surveys %>%
+  filter(sex == "F" | sex == "M") %>%
+  group_by(sex, species_id) %>%
+  summarize(mean_weight = mean(weight, na.rm = T),
+            min_weight = min(weight, na.rm = T))
+
+## tally counts the total number of observations for the variable
+surveys %>%
+  group_by(sex) %>%
+  tally
 
 
